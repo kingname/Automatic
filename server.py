86886544e05969f51637508c851a6d3c2df5ddb6
谢乾坤 <kingname@kingname.local>
2016-11-07 20:07:25 +0800

    [20161107] init repo
    [description]
    this is a web server for my automatic project.
    and the first project is an automatic alarm clock.

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from datetime import date
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/alarm_clock")
+def alarm_clock():
+    if os.path.exists('alarmclock.txt'):
+        with open('alarmclock.txt') as f:
+            date_in_txt = f.read()
+            today = str(date.today())
+            if date_in_txt == today:
+                return 'yes'
+    return 'no'
+
+
+@app.route('/set_alarm')
+def set_alarm():
+    with open('alarmclock.txt', 'w') as f:
+        f.write(str(date.today()))
+    return 'OK'
+
+
+if __name__ == "__main__":
+    app.run()
