a77711a10fb75ab44347617bee39ba9768e78824
kingname <greensouth@foxmail.com>
2017-01-07 13:43:32 +0800

    update threader

--- BackEnd/run.py
+++ BackEnd/run.py
@@ -4,5 +4,5 @@ from BackEnd.src.Views import *
 if __name__ == "__main__":
     host = app.config['HOST']
     port = app.config['PORT']
-    app.run(host=host, port=port)
-    # app.run(processes=10)
+    app.run(host=host, port=port, threaded=True)
+    app.run(processes=10)
